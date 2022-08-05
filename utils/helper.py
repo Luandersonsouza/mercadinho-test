4b6f7855fb43aa5f73451e995390859e650d890e
Luanderson Souza <70651073+Luandersonsouza@users.noreply.github.com>
2022-08-05 08:50:32 -0300

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,2 @@
-class Moeda:
-
-    def formata_float_str_moeda(valor: float) -> str:
-        return f'R$ {valor:,.2f}'
+def formata_float_str_moeda(valor: float) -> str:
+    return f'R$ {valor:,.2f}'
